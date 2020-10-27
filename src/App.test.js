0b44a8b2c69f2bc5a09cf2166f6a280aab4ec944
Chyngyz Sydykov <chyngyz6@gmail.com>
2020-10-28 00:40:02 +0600

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World Chika/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders wow text', () => {
+  render(<App />);
+  const ddd = screen.getByText(/wow/i);
+  expect(ddd).toBeInTheDocument();
 });
